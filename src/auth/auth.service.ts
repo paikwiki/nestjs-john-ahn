657fc3967f4a36cede3965b415038fde3d73f8a2
paikwiki <paikwiki@gmail.com>
2024-09-18 22:58:16 +0900

    :sparkles: Done 5:15:35
    커스텀 데코레이터 생성하기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
     if (user && (await bcrypt.compare(password, user.password))) {
       // create user token ( Secret + Payload )
       const payload = { username };
-      const accessToken = await this.jwtService.sign(payload);
+      const accessToken = this.jwtService.sign(payload);
 
       return { accessToken };
     } else {
